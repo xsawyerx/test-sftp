88bf1669c86d1fdbe27546023bcb1d7cfeb120c3
Sawyer X <xsawyerx@cpan.org>
2010-06-03 15:20:47 +0300

    updating POD

--- lib/Test/SFTP.pm
+++ lib/Test/SFTP.pm
@@ -16,26 +16,22 @@ our $VERSION = '1.05';
 
 # variables for the connection
 has 'host'     => ( is => 'ro', isa => 'Str', required => 1 );
-has 'user'     => ( is => 'ro', isa => 'Str' );
-has 'password' => ( is => 'ro', isa => 'Str' );
+has 'user'     => ( is => 'ro', isa => 'Str'                );
+has 'password' => ( is => 'ro', isa => 'Str'                );
 
-has 'debug' => ( is => 'ro', isa => 'Int', default => 0 );
-has 'warn'  => ( is => 'ro', isa => 'Int', default => 0 );
-
-has 'more' => ( is => 'rw', isa => 'ArrayRef' );
+has 'debug'    => ( is => 'ro', isa => 'Int', default => 0  );
+has 'timeout'  => ( is => 'ro', isa => 'Int'                );
+has 'more'     => ( is => 'ro', isa => 'ArrayRef'           );
 
 # this holds the object itself. that way, users can do:
 # $t_sftp->object->get() in a raw manner if they want
 has 'object' => (
-    is         => 'rw',
+    is         => 'ro',
     isa        => 'Net::SFTP::Foreign',
     lazy_build => 1,
 );
 
-has 'connected'    => ( is => 'rw', isa => 'Bool', default => 0 );
-has 'auto_connect' => ( is => 'rw', isa => 'Bool', default => 1 );
-
-has 'timeout'      => ( is => 'ro', isa => 'Int' );
+has 'connected' => ( is => 'rw', isa => 'Bool', default => 0 );
 
 my $CLASS = __PACKAGE__;
 
@@ -87,18 +83,18 @@ sub cannot_connect {
     $tb->ok( $self->object->error, $test );
 }
 
-sub is_error {
-    my ( $self, $error, $test ) = @_;
-    my $tb = $CLASS->builder;
-
-    $tb->is_eq( $self->object->error, $error, $test );
-}
-
 sub is_status {
     my ( $self, $status, $test ) = @_;
     my $tb = $CLASS->builder;
 
     $tb->is_eq( $self->object->status, $status, $test );
+}
+
+sub is_error {
+    my ( $self, $error, $test ) = @_;
+    my $tb = $CLASS->builder;
+
+    $tb->is_eq( $self->object->error, $error, $test );
 }
 
 sub can_get {
@@ -164,7 +160,7 @@ __END__
 
 =head1 NAME
 
-I<Test::SFTP> - An object to help test Net::SFTP
+Test::SFTP - An object to help test SFTPs
 
 =head1 SYNOPSIS
 
@@ -177,7 +173,7 @@ I<Test::SFTP> - An object to help test Net::SFTP
         ...
     );
 
-    $t_sftp->can_get( $remote_path, "Trying to get: $remote_path" );
+    $t_sftp->can_get( $remote_path, $local_path, "Getting $remote_path" );
 
     $t_sftp->can_put(
         $local_path,
@@ -187,177 +183,153 @@ I<Test::SFTP> - An object to help test Net::SFTP
 
 =head1 VERSION
 
-This describes I<Test::SFTP> 1.05.
+This describes Test::SFTP 1.05.
 
 =head1 DESCRIPTION
 
 Unlike most testing frameworks, I<Test::SFTP> provides an object oriented
 interface. The reason is that it's simply easier to use an object than throw the
-login information as command arguments each time. Maybe in time, there will be
-another interface that will accept login information through global package
-variables.
+login information as command arguments each time.
 
-I<Test::SFTP> uses I<Net::SFTP> for the SFTP functions. This is actually a
-testing framework for I<Net::SFTP>.
 
 =head1 ATTRIBUTES
 
-Basically there is almost complete corrolation with I<Net::SFTP> attributes,
-except for a few changes here and there. Since these are attributes, you can set
-all of these from the C<< $t_sftp->new() >> method.
+Most attributes (at least those you can set on initialization) are read-only.
+That means they cannot be set after the object was already created.
 
     $t_sftp->new(
-        host         => 'localhost',
-        user         => 'root'
-        password     => 'p455w0rdZ'
-        debug        => 1     # default: 0
-        warn         => 1     # default: 0
-        more         => [ qw( -o PreferredAuthentications=password ) ]
-        auto_connect => 0     # default: 1
-        timeout      => 10    # 10 seconds timeout for the connection
+        host     => 'localhost',
+        user     => 'root'
+        password => 'p455w0rdZ'
+        debug    => 1     # default: 0
+        more     => [ qw( -o PreferredAuthentications=password ) ]
+        timeout  => 10    # 10 seconds timeout for the connection
     );
 
-=head2 $t_sftp->host($host)
+=head2 host
 
 The host you're connecting to.
 
-=head2 $t_sftp->user($username)
+=head2 user
 
 Username you're connecting with.
 
-=head2 $t_sftp->password($password)
+If you do not specify this explicitly, it will use the user who is running
+the application.
+
+=head2 password
 
 Password for the username you're connecting with.
 
-=head2 $t_sftp->debug($boolean)
+If you do not specify this explicitly, it will try other connection methods
+such as SSH keys.
 
-Debugging flag for I<Net::SFTP>. Haven't used it yet, don't know if it will ever
-come in handy.
+=head2 debug
 
-=head2 $t_sftp->warn($boolean)
+This flag turns on verbose for I<Net::SFTP::Foreign>.
 
-Warning flag for I<Net::SFTP>. Haven't used it yet, don't know if it will ever
-come in handy.
+=head2 more
 
-=head2 $t_sftp->more( [ @args ] )
+SSH arguments, such as used in I<Net::SFTP::Foreign>, I<Net::OpenSSH> or plain
+OpenSSH.
 
-SSH arguments, such as used in I<Net::SFTP>. These are actually for
-I<Net::SSH::Perl>.
+=head2 timeout
 
-=head2 $t_sftp->auto_connect($boolean)
+This turns on both connection timeout (via I<-o ConnectTimeout=$time>) for ssh
+and a timeout for every data request.
 
-Some methods require a connection which is monitored by an internal attribute
-listed below. This method can alter that behavior, dictating that Test::SFTP
-should not issue a connection if it's not connected. The default is to issue a
-connection if C<< $t_sftp->connected >> returns false.
-
-=head2 $t_sftp->timeout($seconds)
-
-When you want to make sure the login to SFTP won't hang, you can set a timeout.
-However, it applies to the login only, and not to any other method.
+It is recommended to set a timeout, or the test might hang for a very long time
+if the target is unavailable.
 
 =head2 Sensitive Attributes
 
 =over 4
 
-=item C<< $t_sftp->connected($boolean) >>
+=item connected
 
-A boolean attribute to note whether the I<Net::SFTP> object is connected.
+A boolean attribute to note whether the I<Net::SFTP::Foreign> object is
+connected.
 
 Most methods used need the object to be connected. This attribute is used
-internally to check if it's not connected yet, and if it isn't, it will run the
-connect method again in order to connect. This behavior can be altered using the
-previous attribute C<< $t_sftp->auto_connect >>.
+internally to check if it's not connected yet, and if it isn't, it reconnect.
+
+You can use this attribute to check whether it's connected internally in your
+test script or run it using I<< $t_sftp->is_connected >> as a test.
+
+However, try not to set this attribute.
 
 =item C<< $t_sftp->object($object) >>
 
-This holds the object of I<Net::SFTP>. It's there to allow users more
+This holds the object of I<Net::SFTP::Foreign>. It's there to allow users more
 fingergrain access to the object. With that, you can do:
 
     is(
-        $t_sftp->object->do_read( ... ),
+        $t_sftp->object->some_method( ... ),
         'Specific test not covered in the framework',
     );
 
-You can change this to a different object you want to use instead of
-I<Net::SFTP>, but the API should be as close to it as possible. Goodluck!
+Please refer to L<Net::SFTP::Foreign> for all the attributes and methods it
+supports.
 
 =back
 
 =head1 SUBROUTINES/METHODS
 
-=head2 $t_sftp->connect
-
-Test::SFTP does not connect when it's created. You should explicitly connect
-using:
-
-    $t_sftp->connect
-
-Then you could use the available testing methods described below.
-
-If the auto_connect attribute (which is set by default) is on, it will connect
-as soon as a testing method is used and it finds out it isn't connected already.
-
 =head2 $t_sftp->can_connect($test_name)
 
-Checks whether we were able to connect to the machine. It basically runs the
-connect method, but checks if it was successful with a test name.
+Checks whether we were able to connect to the machine.
 
 =head2 $t_sftp->cannot_connect($test_name)
 
-Checks whether we were NOT able to connect to the machine. Runs the connect
-method adn checks if it unsuccessful with a test name.
+Checks whether we were B<not> able to connect to the machine.
 
-=head2 $t_sftp->is_status( "$number $string" , $test_name )
+=head2 $t_sftp->is_status( $string , $test_name )
 
-Checks the status of I<Net::SFTP>. It's the same as
-C<< is( "$expected_number $expected_string", Test::SFTP->object->status,
-'testing the status returned by Net::SFTP) >>.
+Checks the status code returned from the SFTP server.
 
-This returns the entire string back. It joins both the error number and the
-FX2TXT, joined by a space character.
+This is practicely the FX2TXT.
 
-=head2 $t_sftp->is_status_number( $number, $test_name )
+=head2 $t_sftp->is_error( $string , $test_name )
 
-Returns the status number, the first part of the whole status.
+Checks for a certain SFTP error existing.
 
-=head2 $t_sftp->is_status_string( $string, $test_name )
+=head2 $t_sftp->can_get( $remote, $local, $test_name )
 
-Returns the FX2TXT part of the status.
+Checks whether we're able to get a file from C<$remote> to C<$local>.
 
-=head2 $t_sftp->can_get( $filename, $test_name )
+=head2 $t_sftp->cannot_get( $remote, $local, $test_name )
 
-Checks whether we're able to get a file.
+Checks whether we're unable to get a file from C<$remote> to C<$local>.
 
-=head2 $t_sftp->cannot_get( $filename, $test_name )
+=head2 $t_sftp->can_put( $local, $remote, $test_name )
 
-Checks whether we're unable to get a file.
+Checks whether we're able to upload a file from C<$local> to C<$remote>.
 
-=head2 $t_sftp->can_put( $filename, $test_name )
+=head2 $t_sftp->cannot_put( $local, $remote, $test_name )
 
-Checks whether we're able to upload a file.
+Checks whether we're unable to upload a file from C<$local> to C<$remote>.
 
-=head2 $t_sftp->cannot_put( $filename, $test_name )
-
-Checks whether we're unable to upload a file.
-
-=head2 $t_sftp->can_ls( $filename, $test_name )
+=head2 $t_sftp->can_ls( $path, $test_name )
 
 Checks whether we're able to ls a folder or file. Can be used to check the
 existence of files or folders.
 
-=head2 $t_sftp->cannot_ls( $filename, $test_name )
+=head2 $t_sftp->cannot_ls( $path, $test_name )
 
 Checks whether we're unable to ls a folder or file. Can be used to check the
 nonexistence of files or folders.
 
 =head1 DEPENDENCIES
 
-L<http://search.cpan.org/perldoc?Moose>
+L<Moose>
 
-L<http://search.cpan.org/perldoc?Net::SFTP>
+L<Net::SFTP::Foreign>.
 
-L<http://search.cpan.org/perldoc?Test::More>
+L<Test::Builder>
+
+L<namespace::autoclean>
+
+L<parent>
 
 =head1 AUTHOR
 
@@ -426,14 +398,11 @@ L<http://search.cpan.org/dist/Test-SFTP/>
 
 =back
 
-
 =head1 ACKNOWLEDGEMENTS
-
-Dave Rolsky and David Robins for maintaining I<Net::SFTP>.
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2009 Sawyer X, all rights reserved.
+Copyright 2009-2010 Sawyer X, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
